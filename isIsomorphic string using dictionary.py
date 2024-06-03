4f1e60c6806b8189c538f60c2023d7636d48a517
HASAN RAFI AHMED <108696119+Rafi570@users.noreply.github.com>
2024-06-03 20:07:36 +0600

    Create isIsomorphic string using dictionary.py

---
+++ isIsomorphic string using dictionary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dict = {}
+        hSet = set()
+
+        for i in range (len(s)):
+            sChar = s[i]
+            tChar = t[i]
+            if (sChar not in dict):
+                if(tChar in hSet):
+                    return False
+                
+                dict[sChar] = tChar
+                hSet.add(tChar)
+            
+            else:    #check if exists
+                if(tChar != dict[sChar]):
+                    return False
+
+        return True
+ob=Solution()
+print(ob.isIsomorphic("badc","baba"))
